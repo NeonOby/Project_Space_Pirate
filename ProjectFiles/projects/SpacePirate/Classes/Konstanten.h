e46f37209059a0224bf3e56b64f391272690e8e8
ADoby <zimmerlint@gmail.com>
2013-09-10 14:21:55 +0200

    Hallo

--- ProjectFiles/projects/SpacePirate/Classes/Konstanten.h
+++ ProjectFiles/projects/SpacePirate/Classes/Konstanten.h
@@ -4,18 +4,25 @@
 //Umrechnung von Box2D Metern (Box2D Einheit) zu CoCos2D Points (Pixeln)
 #define PTM_RATIO 64.0f
 
+//So Zeug wie maximale Geschwindigkeit \
+//Das man geduckt langsamer läuft
+//Kletter-Geschwindigkeit
 #pragma region PLAYER_VARIABLES
 
+//Maximale Geschwindigkeit (Wird mit Gewicht multipliziert)
 #define PLAYER_MAX_SPEED 6.0f
 
-#pragma endregion	//Maximale Geschwindigkeit \
-					Multiplier wenn geduckt \
-					Ja
+//Geschwindigkeits-Änderung, also wie schnell man die Richtung wechselt etc.
+#define PLAYER_SPEED_CHANGE = 5.0f
+
+#pragma endregion
+
+#pragma region PLAYER_FALL_VARIABLES
 
 //Fallzeit * this = Zeit in Sekunden für den Debuff (Langsamer Laufen)
 #define FALL_DEBUF_TIME_MULTIPLIER 0.6f
 //MaxSpeed * this = Maximale Geschwindigkeit die der Spieler noch haben kann mit Debuff (Langsamer Laufen)
-#define FALL_DEBUF_SPEED_MULTIPLIER  0.5f
+#define FALL_DEBUF_SPEED_MULTIPLIER 0.5f
 
 //Fall-Zeit in Sekunden ab wann der Spieler schaden bekommt und stehen bleibt
 #define FALL_TIME_MAX_WITHOUT_DAMAGE 0.5f
@@ -24,15 +31,17 @@
 //Fallzeit * this = Zeit in Sekunden die der Spieler stehen bleibt;
 #define FALL_STOP_TIME_MULTIPLIER 0.15f
 
+#pragma endregion
+
 enum EffectSizes {
-    IMPULSE_SIZE =          24
+	IMPULSE_SIZE =          24
 };
 
 enum EntityGroups {
-    SHIP =				0x0001,
-    ASTEROID =			0x0002,
-    ENEMY =				0x0004,
-    IMPULSE =			0x0008
+	SHIP =				0x0001,
+	ASTEROID =			0x0002,
+	ENEMY =				0x0004,
+	IMPULSE =			0x0008
 };
 
 class Konstanten{
