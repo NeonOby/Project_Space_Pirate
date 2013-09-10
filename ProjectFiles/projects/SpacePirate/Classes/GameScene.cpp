7fac5b9287cae03d4bffc88ece9896ce821c5656
ADoby <zimmerlint@gmail.com>
2013-09-10 14:17:44 +0200

    Joai

--- ProjectFiles/projects/SpacePirate/Classes/GameScene.cpp
+++ ProjectFiles/projects/SpacePirate/Classes/GameScene.cpp
@@ -324,8 +324,6 @@ void GameScene::update(float dt){
         }
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	//Jump
 	if(!jumping && GetAsyncKeyState(VK_SPACE)){
 		_Player->ApplyLinearImpulse(b2Vec2(0.0f, 60.0f), _Player->GetWorldCenter());
@@ -336,15 +334,14 @@ void GameScene::update(float dt){
 		if(_Player->GetLinearVelocity().y == 0){
 			jumping = false;
 		}
-=======
-=======
+
+	}
+
 	bool canMove = true;
->>>>>>> origin/Tobias
 	bool JUMP_PRESSED = false;
 
 	if(GetAsyncKeyState(VK_SPACE)){
 		JUMP_PRESSED=true;
->>>>>>> origin/Tobias
 	}
 
 	float maxSpeed = 5.0f;
